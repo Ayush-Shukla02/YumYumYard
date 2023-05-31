efeb17df900806ccfdad33d92faa666aaf9f63e8
sleepinGiant <shukla.ayush0203@gmail.com>
2023-05-31 17:18:49 +0530

    Changes: Dashboard, Uploading Items

--- client/src/components/DBItems.jsx
+++ client/src/components/DBItems.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { DataTable } from "../components";
 
 const DBItems = () => {
-	return <div>DBItems</div>;
+	return (
+		<div className="flex items-center justify-center gap-4 pt-5 w-full">
+			<DataTable />
+		</div>
+	);
 };
 
 export default DBItems;
