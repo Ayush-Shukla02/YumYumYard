30a422cf55c098e821ae9a039231cd3346c4e836
sleepinGiant <shukla.ayush0203@gmail.com>
2023-06-01 16:15:53 +0530

    First Part of Home

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { Delivery } from "../assets";
+import { buttonClick } from "../animations";
 
 const Home = () => {
 	return (
@@ -17,6 +18,22 @@ const Home = () => {
 						/>
 					</div>
 				</div>
+				<p className="text-[40px] text-headingColor md:text-[72px] font-sans font-extrabold tracking-wider">
+					Fastest Delivery in{" "}
+					<span className="text-orange-600">Your City</span>
+				</p>
+				<p className="text-textColor text-lg">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. A,
+					voluptates qui molestiae, et numquam recusandae explicabo
+					ducimus pariatur ea consequuntur quasi tempora. Reiciendis
+					delectus laudantium rem aut eum perspiciatis nobis?
+				</p>
+				<motion.button
+					{...buttonClick}
+					className="bg-gradient-to-bl from-orange-400 to-orange-600 px-4 py-2 rounded-xl text-black text-base font-semibold"
+				>
+					Order Now
+				</motion.button>
 			</div>
 			<div className="">2</div>
 		</motion.div>
