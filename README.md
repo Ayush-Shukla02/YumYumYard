f72153073a2b563f8ee991ac0475b1dff7192cff
sleepinGiant <shukla.ayush0203@gmail.com>
2023-06-05 17:54:16 +0530

    Add Items to Cart

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # YumYumYard
 
-
 ### Issues:
-- Dashboard:
-  - Products state does not load
-  - Users state does not load
-  - Check by doing console.log() the data
-  - It shows error in Material Table - ```this.props is not a function```
+
+-   Dashboard:
+
+    -   Products state does not load
+    -   Users state does not load
+    -   Check by doing console.log() the data
+    -   It shows error in Material Table - `this.props is not a function`
+
+-   Overall
+    -   Check the logged in user handler function as data of the user is present in `user.data` and not in `user`
--- client/src/components/SliderCard.jsx
+++ client/src/components/SliderCard.jsx
@@ -2,8 +2,29 @@ import React from "react";
 import { buttonClick } from "../animations";
 import { motion } from "framer-motion";
 import { IoBasket } from "../assets/icons";
+import { addNewItemToCart, getAllCartItems } from "../api";
+import { useDispatch, useSelector } from "react-redux";
+import { alertNull, alertSuccess } from "../context/actions/alertActions";
 
 const SliderCard = ({ data, index }) => {
+	const user = useSelector((state) => state.user);
+
+	const dispatch = useDispatch();
+
+	const sendToCart = () => {
+		addNewItemToCart(user?.data.user_id, data).then((res) => {
+			dispatch(alertSuccess("Item added to cart"));
+
+			getAllCartItems(user?.data.user_id).then((items) => {
+				console.log(items);
+			});
+
+			setInterval(() => {
+				dispatch(alertNull());
+			}, 3000);
+		});
+	};
+
 	return (
 		<div className="bg-lightOverlay hover:drop-shadow-lg backdrop-blur-md rounded-xl flex items-center justify-between relative px-4 py-2 w-full md:w-340 md:min-w-350 gap-3">
 			<img src={data.imageURL} className="w-40 h-40 object-contain" />
@@ -16,6 +37,7 @@ const SliderCard = ({ data, index }) => {
 				</p>
 				<motion.div
 					{...buttonClick}
+					onClick={sendToCart}
 					className="w-8 h-8 rounded-full bg-red-500 flex items-center justify-center absolute -top-4 right-2 cursor-pointer"
 				>
 					<IoBasket className="text-primary text-2xl" />
