8f9072055f6c83154ea5ee4345813ef06c7cf16e
sleepinGiant <shukla.ayush0203@gmail.com>
2023-06-09 19:35:46 +0530

    Cart UI Fix

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -149,7 +149,9 @@ export const CartItemCard = ({ index, data }) => {
 				>
 					<p className="text-xl font-semibold text-primary">-</p>
 				</motion.div>
-				<p>{data.quantity}</p>
+				<p className="text-lg text-primary font-semibold">
+					{data?.quantity}
+				</p>
 				<motion.div
 					{...buttonClick}
 					onClick={() => incrementCart(data?.productId)}
